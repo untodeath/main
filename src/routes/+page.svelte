346669bc2117ab5e9fd6e4a409d83ebc5b417e98
Austin <git@austinriddell.com>
2022-09-29 01:20:14 -0500

    Set my paths straight Lord

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<h1>WIP</h1>
+
+<style>
+	@import url('https://rsms.me/inter/inter.css');
+
+	:root {
+		box-sizing: border-box;
+	}
+
+	*,
+	*::before,
+	*::after {
+		appearance: none;
+		box-sizing: inherit;
+		margin: 0;
+		padding: 0;
+	}
+
+	h1 {
+		align-items: center;
+		display: flex;
+		font-family: 'Inter', sans-serif;
+		font-size: 10rem;
+		font-weight: 900;
+		height: 100vh;
+		justify-content: center;
+		line-height: 1;
+		text-align: center;
+	}
+
+	@supports (font-variation-settings: normal) {
+		h1 {
+			font-family: 'Inter var', sans-serif;
+		}
+	}
+</style>
